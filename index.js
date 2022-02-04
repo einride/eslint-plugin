96741ea0930845ad47d8543cfb0851886e12ee9b
Filip Tammergård <filip@tammergard.se>
2022-02-04 10:17:22 +0100

    feat: ignore config file

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
       parserOptions: {
         project: "./tsconfig.json",
       },
+      ignorePatterns: [".eslintrc.js"],
       extends: [
         "plugin:jest/recommended",
         "airbnb",
